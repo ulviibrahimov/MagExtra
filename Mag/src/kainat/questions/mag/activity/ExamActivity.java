4c9a349da9f14a9c755427f432e0156d898c3cfe
ulviibrahimov <ulvi@ualberta.ca>
2013-12-28 01:35:05 -0600

    exam bugs fixed

--- Mag/src/kainat/questions/mag/activity/ExamActivity.java
+++ Mag/src/kainat/questions/mag/activity/ExamActivity.java
@@ -19,6 +19,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -26,77 +27,37 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class ExamActivity extends Activity {
-	List<String> userAnswers=new ArrayList<String>();
-	List<Question> examQuestionList= new ArrayList<Question>();
-	Integer examQuestionIndex=0;
-	Integer startEnglish=0;
-	Integer startFrench=0;
-	Integer startInformatics=0;
-	Integer startRussian=0;
-	Integer startLogicText=0;
-	Integer startLogicImage=0;
-	Integer langChoosen=0;
-	Integer startLang=0;
-	Integer lightGray=-3355444;
-	Integer blue=-16776961;
+	public static List<String> userAnswers=new ArrayList<String>();
+	public static List<Question> examQuestionList;
+	public static Integer examQuestionIndex;
+	public static Integer startEnglish;
+	public static Integer startFrench;
+	public static Integer startInformatics;
+	public static Integer startRussian;
+	public static Integer startLogicText;
+	public static Integer startLogicImage;
+	public static Integer langChoosen;
+	public static Integer startLang;
+	public static Integer lightGray=-3355444;
+	public static Integer blue=-16776961;
 	Context context=this;
 	Button aButton;
 	Button bButton;
 	Button cButton;
-	Button dButton;
+	Button dButton;		
 	Button eButton;
 	TextView t;
 	TextView timer;
+	public static boolean examFirstRun=true;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_exam);
-
-		examQuestionList=QuestionsActivity.examQuestionL;
-		timer=new TextView(this);
-		timer=(TextView)findViewById(R.id.timerView);
-		new CountDownTimer(9000000, 10000) {
-			 
-		     public void onTick(long millisUntilFinished) {
-		    	 long minutes= millisUntilFinished / 60000;
-		    	 int showHours=(int) (minutes/60);
-		    	 int showMinutes= (int) (minutes-60*showHours+1);
-		    	 if(showHours>0){
-		    		 
-		    		 timer.setText("Vaxt: " + showHours+"s "+showMinutes+"d");
-		    	 }
-		    	 else{
-		    		 timer.setText("Vaxt: " +showMinutes+" d");
-		    	 }
-		     }
-
-		     public void onFinish() {
-		         showResult();
-		     }
-		  }.start();
-		System.out.println("examsize:"+examQuestionList.size());
-		for(int p=0;p<examQuestionList.size();p++){
-			userAnswers.add(null);
-			examQuestionList.get(p).setColorA(lightGray);
-			examQuestionList.get(p).setColorB(lightGray);
-			examQuestionList.get(p).setColorC(lightGray);
-			examQuestionList.get(p).setColorD(lightGray);
-			examQuestionList.get(p).setColorE(lightGray);
-		}
+		Window w= getWindow();
+		w.setTitle("Sınaq");
 		
-		this.displayQuestion(examQuestionList.get(0), 0);	
-		startLogicText=ExamHelper.getStartText(0,examQuestionList);
-		startInformatics=ExamHelper.getStartInformatics(startLogicText, examQuestionList);
-		startEnglish=ExamHelper.getStartEnglish(startInformatics, examQuestionList);
-		startRussian=ExamHelper.getStartRussian(startEnglish, examQuestionList);
-		startFrench=ExamHelper.getStartFrench(startRussian, examQuestionList);
 		
-		if(startEnglish>0)
-			startLang=startEnglish;
-		else if(startRussian>0)
-			startLang=startRussian;
-		else if(startFrench>0)
-			startLang=startFrench;
+
 		addListenerOnButtonPrev();
 		addListenerOnButtonNext();
 		addListenerOnButtonA();
@@ -105,7 +66,120 @@ public class ExamActivity extends Activity {
 		addListenerOnButtonD();
 		addListenerOnButtonE();
 	}
+	@Override
+	protected void onResume()
+	{
+	   super.onResume();
+	   if(examFirstRun){
+		    langChoosen=0;
+			startLogicText=0;
+			startInformatics=0;
+			startEnglish=0;
+			startRussian=0;
+			startFrench=0;
+			examQuestionIndex=0;
+			examQuestionList= new ArrayList<Question>();
 
+           ExamHelper.addQuestions(QuestionsActivity.questionL, examQuestionList, 0, QuestionsActivity.startLogicText, 15);			//image questions
+           ExamHelper.addQuestions(QuestionsActivity.questionL, examQuestionList, QuestionsActivity.startLogicText, QuestionsActivity.startInformatics, 35); //text logic
+           ExamHelper.addQuestions(QuestionsActivity.questionL, examQuestionList, QuestionsActivity.startInformatics, QuestionsActivity.startEnglish,25);//informatics
+           System.out.println("size"+examQuestionList);
+    	   for(int p=0;p<examQuestionList.size();p++){
+    				userAnswers.add(null);
+    				examQuestionList.get(p).setColorA(lightGray);
+    				examQuestionList.get(p).setColorB(lightGray);
+    				examQuestionList.get(p).setColorC(lightGray);
+    				examQuestionList.get(p).setColorD(lightGray);
+    				examQuestionList.get(p).setColorE(lightGray);
+    			}
+		AlertDialog.Builder langAlert;
+	    final CharSequence[] languages = {"Ingilis dili","Rus dili","Fransız dili"};
+	    langAlert = new AlertDialog.Builder(context);
+	    langAlert.setCancelable(false);
+	    langAlert.setTitle("Xarici dil seçin");
+	    langAlert.setSingleChoiceItems(languages,-1, new DialogInterface.OnClickListener()
+	    {
+	        @Override
+	        public void onClick(DialogInterface dialog, int which) 
+	        {		//System.out.println("examsize:"+examQuestionList.size());
+	            if(languages[which]=="Ingilis dili")
+	            {
+	            	langChoosen=1;
+	            	
+	            	}
+	            else if (languages[which]=="Rus dili")
+	            {	langChoosen=2;
+	            	
+	            }
+	            else if (languages[which]=="Fransız dili")
+	            {
+	            	langChoosen=3;
+	            	
+	            }
+	        }
+	    });		
+	    langAlert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+	    	public void onClick(DialogInterface dialog, int whichButton) {
+	    		if(langChoosen==0){
+	    			ExamHelper.addQuestions(QuestionsActivity.questionL, examQuestionList, QuestionsActivity.startEnglish, QuestionsActivity.startRussian,25);    //english
+	    		}
+	    		if(langChoosen==1){
+	    			ExamHelper.addQuestions(QuestionsActivity.questionL, examQuestionList, QuestionsActivity.startEnglish, QuestionsActivity.startRussian,25);    //english
+	    		}	               
+	    		else if(langChoosen==2){
+	    			ExamHelper.addQuestions(QuestionsActivity.questionL, examQuestionList, QuestionsActivity.startRussian, QuestionsActivity.startFrench,25);	  //russian
+	    		}
+	    		else if(langChoosen==3){
+	    			ExamHelper.addQuestions(QuestionsActivity.questionL, examQuestionList, QuestionsActivity.startFrench, QuestionsActivity.questionL.size(),25); //french
+	    		}
+	    	
+	    		System.out.println("eqlist"+examQuestionList.size());
+	    		for(int p=0;p<examQuestionList.size();p++){
+					userAnswers.add(null);
+					examQuestionList.get(p).setColorA(lightGray);
+					examQuestionList.get(p).setColorB(lightGray);
+					examQuestionList.get(p).setColorC(lightGray);
+					examQuestionList.get(p).setColorD(lightGray);
+					examQuestionList.get(p).setColorE(lightGray);
+				}
+	    		System.out.println("size"+examQuestionList.size());
+			    startLogicText=ExamHelper.getStartText(0,examQuestionList);
+				startInformatics=ExamHelper.getStartInformatics(startLogicText, examQuestionList);
+				startEnglish=ExamHelper.getStartEnglish(startInformatics, examQuestionList);
+				startRussian=ExamHelper.getStartRussian(startEnglish, examQuestionList);
+				startFrench=ExamHelper.getStartFrench(startRussian, examQuestionList);
+				if(startEnglish>0)
+					startLang=startEnglish;
+				else if(startRussian>0)
+					startLang=startRussian;
+				else if(startFrench>0)
+					startLang=startFrench;
+	    	}
+	    	});	
+	    langAlert.show();
+	    timer=new TextView(this);
+		timer=(TextView)findViewById(R.id.timerView);
+		new CountDownTimer(9000000, 10000) {
+			 
+		     public void onTick(long millisUntilFinished) {
+		    	 long minutes= millisUntilFinished / 60000;
+		    	 timer.setText("Vaxt: " +(minutes+1)+" dəq");
+		    	 
+		     }
+
+		     public void onFinish() {
+		         showResult();
+		     }
+		  }.start();
+      		
+			examFirstRun=false;
+		}
+	   System.out.println("examindex: "+examQuestionIndex);
+	   System.out.println("examsize: "+examQuestionList.size());
+	   this.displayQuestion(examQuestionList.get(examQuestionIndex), examQuestionIndex);
+	   
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -121,6 +195,7 @@ public class ExamActivity extends Activity {
     	exitAlert.setMessage("Sınağı tərk edəcəyiniz halda nəticəniz hesablanmayacaq! Çıxmaq istədiyinizdən əminsinizmi?");
     	exitAlert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
     	public void onClick(DialogInterface dialog, int whichButton) {
+    		examFirstRun=true;
     		ExamActivity.this.finish();
             //startActivity(intent);
     	  }
@@ -197,9 +272,11 @@ public class ExamActivity extends Activity {
 	        	AlertDialog.Builder exitAlert = new AlertDialog.Builder(this);
 
 	        	exitAlert.setTitle("Çıxış");
+	        	exitAlert.setCancelable(false);
 	        	exitAlert.setMessage("Sınağı tərk edəcəyiniz halda nəticəniz hesablanmayacaq! Çıxmaq istədiyinizdən əminsinizmi?");
 	        	exitAlert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
 	        	public void onClick(DialogInterface dialog, int whichButton) {
+	        		examFirstRun=true;
 	        		ExamActivity.this.finish();
 		            //startActivity(intent);
 	        	  }
@@ -229,6 +306,7 @@ public class ExamActivity extends Activity {
 			final int id = getResources().getIdentifier("kainat.questions.mag:drawable/" + imageName, null, null);
 			System.out.println("from id: "+id);
 			img.setImageResource(id);
+			img.setClickable(true);
 			img.setOnClickListener(new View.OnClickListener(){
 			    public void onClick(View v) {
 			        Intent imageIntent = new Intent(ExamActivity.this,ImageActivity.class);
@@ -240,6 +318,7 @@ public class ExamActivity extends Activity {
 		else{
 			ImageView img= (ImageView) findViewById(R.id.imageView1);
 			img.setImageResource(R.drawable.kainat);
+			img.setClickable(false);
 		}
 		t=new TextView(this);
 		
@@ -441,6 +520,7 @@ public class ExamActivity extends Activity {
         Integer logicNotAnswered=0;
         Integer informaticsNotAnswered=0;
         Integer langNotAnswered=0;
+        if(startInformatics>0){
         	for(int f=0;f<startInformatics;f++){
         		if(userAnswers.get(f)==null){
         			logicNotAnswered++;
@@ -451,6 +531,8 @@ public class ExamActivity extends Activity {
         		}
         		
         	}
+        }
+        if(startInformatics>0&&startLang>0){
         	for(int f=startInformatics;f<startLang;f++){
         		if(userAnswers.get(f)==null){
         			informaticsNotAnswered++;
@@ -459,6 +541,8 @@ public class ExamActivity extends Activity {
         			informaticsCorrect++;
         		}
         	}
+        }
+        if(startLang>0&&examQuestionList.size()>0){
         	for(int f=startLang;f<examQuestionList.size();f++){
         		if(userAnswers.get(f)==null){
         			langNotAnswered++;
@@ -467,9 +551,9 @@ public class ExamActivity extends Activity {
         			langCorrect++;
         		}
         	}
-        	
+        }
         	AlertDialog.Builder resultAlert = new AlertDialog.Builder(this);
-
+        	resultAlert.setCancelable(false);
         	resultAlert.setTitle("Nəticə");
         	resultAlert.setMessage("Məntiq:  Düzgün: "+ logicCorrect+", səhv: "+(50-logicNotAnswered-logicCorrect)+"\n\n"+
         							"Informatika:  Düzgün: "+ informaticsCorrect+", səhv: "+(25-informaticsNotAnswered-informaticsCorrect)+"\n\n"+
@@ -477,6 +561,7 @@ public class ExamActivity extends Activity {
         							"Toplam:  "+(langCorrect+logicCorrect+informaticsCorrect)+" bal"));
         	resultAlert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
         	public void onClick(DialogInterface dialog, int whichButton) {
+        		examFirstRun=true;
         		ExamActivity.this.finish();
         	  }
         	});
